b02ad6f7b15e5873afd2dc2eb121682952ac43e6
Karan Pratap Singh <synester.gates4@gmail.com>
2022-03-21 18:16:25 +0530

    feat(api): impl

---
+++ api/impl.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"app/service"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
+)
+
+type apiImpl struct {
+	router *mux.Router
+	assets fs.FS
+	svc    service.Service
+}
+
+func New(router *mux.Router, assets fs.FS, svc service.Service) apiImpl {
+	return apiImpl{router, assets, svc}
+}
+
+func (api apiImpl) routes() {
+	api.router.Handle("/", http.FileServer(http.FS(api.assets))).Methods(http.MethodGet)
+	api.router.HandleFunc("/api/buckets", api.ListBuckets).Methods(http.MethodGet)
+}
+
+func (api apiImpl) Start(port int) {
+	api.routes()
+
+	log.Info().Int("port", port).Msg("Starting...")
+	addr := fmt.Sprintf(":%d", port)
+	panic(http.ListenAndServe(addr, api.router))
+}
+
+func (api apiImpl) response(writer http.ResponseWriter, data interface{}) {
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(data)
+}
