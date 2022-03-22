5550b3b74083384ce6eef8bd4fc81628e657eb9d
Karan Pratap Singh <synester.gates4@gmail.com>
2022-03-22 16:37:51 +0530

    deps: tailwind

---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#9E9E9E'
+      }
+    },
+  },
+  plugins: [],
+}
